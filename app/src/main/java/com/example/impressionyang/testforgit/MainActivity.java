3c07e1de90a7c2edc6d588066850f86fcc55a214
foryouthisnight <33059605+foryouthisnight@users.noreply.github.com>
2018-06-08 19:15:24 +0800

    2333

--- app/src/main/java/com/example/impressionyang/testforgit/MainActivity.java
+++ app/src/main/java/com/example/impressionyang/testforgit/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    //2333
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
