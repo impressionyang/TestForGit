9d5b8e013169ae981dae417cf6d586a9f1d3a657
impressionyang <impressionyang@outlook.com>
2018-06-18 23:01:31 +0800

    233

---
+++ app/src/main/java/com/example/impressionyang/testforgit/MainFuvtion/DataBean.java
@@ -0,0 +1,29 @@
+package com.example.impressionyang.testforgit.MainFuvtion;
+
+public class DataBean {
+
+	//233
+	private int num;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	private float tempature;
+	private float fog;
+	public float getTempature() {
+		return tempature;
+	}
+	public void setTempature(float tempature) {
+		this.tempature = tempature;
+	}
+	public float getFog() {
+		return fog;
+	}
+	public void setFog(float fog) {
+		this.fog = fog;
+	}
+	
+	
+}
